0ba8b3c74b245e2ac98f80d84098a4be6e1c56be
kangj <kangjihoon12@naver.com>
2023-06-05 15:02:15 +0900

    백준 코딩테스트

---
+++ src/codingTest2/Main.java
@@ -0,0 +1,9 @@
+package codingTest2;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+    	// System.out.println("Hello World!"); <- Hello World!를 출력하시오.
+    }
+}
